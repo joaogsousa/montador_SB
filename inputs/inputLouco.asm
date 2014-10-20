c2a91b92a40290db830f7444fdf006628886c21c
Joao Henrique <jonibeatles@gmail.com>
2014-10-20 19:25:59 -0200

    testes

---
+++ inputs/inputLouco.asm
@@ -0,0 +1,40 @@
+section data
+
+
+
+;lollolol
+
+
+vetor: space 15
+n1: space
+n2: const 21
+label: equ 0
+aux: equ n1
+
+section text
+Macro_1: macro
+add n1
+sub n2
+input n1
+
+end
+
+
+jmpp rot2
+Macro_1
+if label
+add n1 + 1
+add n1 + 1
+sub n1 - 10
+sub n1 + 5
+load n2 - 13
+add n1 + 150
+add aux
+copy aux, n1
+
+stop
+
+
+
+
+
