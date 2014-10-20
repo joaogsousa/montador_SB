60c13804e4b871c6b08b56b761f9038e568136cb
Daniella Angelos <daniangelos@hotmail.com>
2014-10-20 19:15:40 -0200

    Ultimos ajustes

--- inputs/testemais.asm
+++ inputs/testemais.asm
@@ -1,11 +1,21 @@
-SECTION TEXT
-COPY N1 + 2, N2 - 2 
-jmp lol
-LOAD N1
-ADD N3 + 4
-lol: add N1 
 SECTION DATA
 N1: CONST 3
 N0: CONST 4
 N2: SPACE 3
 N3: SPACE 6
+
+
+
+
+
+
+
+SECTION TEXT
+;comecao codigo
+COPY N1, N2
+jmp lol
+LOAD N1
+ADD N3 + 4
+lol: add N1 ; faz as paradas 
+STORE N2
+OUTPUT N2
